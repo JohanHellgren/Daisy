9e51937a9babefbfd112ef2823b950189d978029
Johan Hellgren <honajnahoj@gmail.com>
2025-09-16 10:02:12 +0200

    start on looper again

--- Johan/MultiLooper/MultiLooper.cpp
+++ Johan/MultiLooper/MultiLooper.cpp
@@ -1,11 +1,14 @@
+#include "Utility/looper.h"
 #include "daisysp.h"
 #include "daisy_pod.h"
-
-#define MAX_SIZE (48000 * 60 * 5)
 
 using namespace daisysp;
 using namespace daisy;
 
 static DaisyPod pod;
 
-
+int main(void)
+{
+    pod.Init();
+    pod.SetAudioBlockSize(4);
+}
